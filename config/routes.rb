ce0b5efe4a9faf87836a89c9a0602c88da7e580a
Eric Dahl <eric@skeyelab.com>
2019-11-12 11:26:59 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,13 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
